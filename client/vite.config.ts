f10c4638150062a98d0f880c7a96f67cedd67833
DjigaS <djiga.salane@quickpay.sn>
2025-03-04 06:28:43 +0000

    daxx

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 80,
+    proxy: {
+      "/v1": "http://localhost:3000"
+    }
+  },
+
   server: {
     port: 3002,
     proxy: {
