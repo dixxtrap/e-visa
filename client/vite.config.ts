95ffc8f83e8e3a42e455dc5197e06313388e4aa9
DjigaS <djiga.salane@quickpay.sn>
2025-03-04 10:58:04 +0000

    _config_host

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   preview: {
-
+    port: 3002,
     allowedHosts:["http://evisa-sn.com"],
     proxy: {
 
