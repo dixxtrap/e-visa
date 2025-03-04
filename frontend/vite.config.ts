9a9eb3fd4dd1492bf8c560e517f480f9da6af367
DjigaS <djiga.salane@quickpay.sn>
2025-03-04 12:27:30 +0000

    _config_host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3001,
+    allowedHosts: true,
+    proxy: {
+      "/v1": "http://localhost:3000",
+    },
+  },
   server: {
     port: 3001,
     proxy: {
-      "/v1": "http://localhost:3000"
-    }
-  }
-})
+      "/v1": "http://localhost:3000",
+    },
+  },
+});
